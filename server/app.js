24fdb499ec20ed64f46c98b9a68b05a347812e41
Lennon-0 <LennonEhare@Gmail.com>
2025-11-05 14:48:45 -0500

    more code, still on domoMakerA

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const helmet = require('helmet');
+
+const router = require('./router.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURI = process.envMONGODB_URI || 'mongodb://localhost/DomoMaker';
+mongoose.connect(dbURI).catch((err) => {
+    if(err){
+        console.log('Could not connect to database');
+        throw err;
+
+    }
+});
+
+const app = express();
+
+app.use(helmet());
+app.use
+app.use
+app.use
+app.use
+app.use
+
+app.engine
+app.set
+app.set
+
+router(app);
+
